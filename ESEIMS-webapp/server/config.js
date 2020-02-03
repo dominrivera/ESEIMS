8bd269c7a7bbc1ec3393774a2be869eeab054f11
dominrivera <dominrivera1@gmail.com>
2020-02-03 12:18:04 +0100

    WEB: Save credentials in env variables file

--- ESEIMS-webapp/server/config.js
+++ ESEIMS-webapp/server/config.js
@@ -1,15 +1,15 @@
 var config = {}
 
-config.port = 3000
+config.port = process.env.PORT || 3000
 
 
 var db_config = {}
 
-db_config.host = 'localhost'
-// db_config.port = 
-db_config.user = 'root'
-db_config.password = ''
-db_config.database = 'eseims'
+db_config.host = process.env.DB_HOST || 'myhost'
+// db_config.port =
+db_config.user = process.env.DB_USER || 'test'
+db_config.password = process.env.DB_PASSWORD || 'test'
+db_config.database = process.env.DB_DATABASE || 'database'
 
 config.db_config = db_config;
 
