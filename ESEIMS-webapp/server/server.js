1dc618b910b5e5e780bcde38f048b127b166730e
dominrivera <dominrivera1@gmail.com>
2020-02-05 21:59:39 +0100

    WEB: Backend Structure

--- ESEIMS-webapp/server/server.js
+++ ESEIMS-webapp/server/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
 const morgan = require('morgan');
+const app = express();
 
-var mysqlConnection = require('./databaseConnection');
 var config = require('./config');
-var app = express();
+//var routes = require('./routes')(app);
 /*var session = require('express-session');
 var Filestore = require('session-file-store')(session);
 
@@ -24,70 +24,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Routes
-app.use('/api', require('./routes'));
+//app.use('/api', routes);
+require('./routes/userRoutes')(app);
 
-
-app.get('/', function (req, res) {
-    res.send('Hello world');
-})
-
-
-/*********************** ROUTES **********************/
-
-// Get list of users
-app.get('/users',(req,res)=>{
-    mysqlConnection.query('SELECT * FROM users',(err, data, fields)=>{
-        if(!err)
-        res.send(data);
-        else
-        console.log(err);
-    });
-});
-
-
-// Get user
-app.get('/users/:id',(req,res)=>{
-    mysqlConnection.query('SELECT * FROM users WHERE id = ?', [req.params.id], (err, data, fields)=>{
-        if(!err)
-        res.send(data);
-        else
-        console.log(err);
-    });
-});
-
-
-// Delete user
-app.delete('/users/:id',(req,res)=>{
-    mysqlConnection.query('DELETE FROM users WHERE id = ?', [req.params.id], (err, data, fields)=>{
-        if(!err)
-        res.send('User deleted');
-        else
-        console.log(err);
-    });
-});
-
-
-// Get list of tickets
-app.get('/tickets',(req,res)=>{
-    mysqlConnection.query('SELECT * FROM tickets',(err, data, fields)=>{
-        if(!err)
-        res.send(data);
-        else
-        console.log(err);
-    });
-});
-
-
-// Get ticket
-app.get('/tickets/:id',(req,res)=>{
-    mysqlConnection.query('SELECT * FROM tickets WHERE id = ?', [req.params.id], (err, data, fields)=>{
-        if(!err)
-        res.send(data);
-        else
-        console.log(err);
-    });
-});
-
+//app.get('/', function (req, res) {
+//    res.send('Hello world');
+//})
 
 // Starting server
 
