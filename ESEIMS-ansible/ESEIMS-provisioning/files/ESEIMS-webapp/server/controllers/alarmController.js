ad4357d6d20923132ae5301c8f98c6e686721762
Domingo Rivera <domingo.rivera.barros@cern.ch>
2020-04-15 20:22:08 +0200

    WEB: Save only alarms with level WARNING or CRITICAL

--- ESEIMS-ansible/ESEIMS-provisioning/files/ESEIMS-webapp/server/controllers/alarmController.js
+++ ESEIMS-ansible/ESEIMS-provisioning/files/ESEIMS-webapp/server/controllers/alarmController.js
@@ -12,29 +12,33 @@ alarmCtrl.listAlarms = function (req, res) {
 
 // save alarms from kapacitor
 alarmCtrl.saveAlarm = function (req, res) {
-    var alarm = req.body
-    var new_alarm = {
-        "type": alarm.id,
-        "value": alarm.message,
-        "level": alarm.level,
-        "host": JSON.stringify(alarm.data.series[0].tags.host),
-        "laboratory": JSON.stringify(alarm.data.series[0].tags.laboratory),
-        "assignment": ''
-    }
-    new_alarm.host = new_alarm.host.slice(1, -1);
-    new_alarm.laboratory = new_alarm.laboratory.slice(1, -1);
-    if (!alarm) {
-        return res.status(400).send({ error: true, message: 'Error saving alarm' });
-    } else {
-        // adding custom fields
-        new_alarm.created = new Date();
-        new_alarm.modified = new Date();
-        new_alarm.status = 'open';
-        Alarm.addAlarm(new_alarm, function (err, new_alarm) {
-            if (err)
-                res.send(err);
-            res.json(new_alarm);
-        });
+    // Only parse alarms with level warning or critical, not interesting to save alarms with level OK
+    if (req.body.level == 'WARNING' || req.body.level == 'CRITICAL') {
+        var alarm = req.body
+        var new_alarm = {
+            "type": alarm.id,
+            "value": alarm.message,
+            "level": alarm.level,
+            "host": JSON.stringify(alarm.data.series[0].tags.host),
+            "laboratory": JSON.stringify(alarm.data.series[0].tags.laboratory),
+            "assignment": ''
+        }
+        new_alarm.host = new_alarm.host.slice(1, -1);
+        new_alarm.laboratory = new_alarm.laboratory.slice(1, -1);
+        console.log(new_alarm);
+        if (!alarm) {
+            return res.status(400).send({ error: true, message: 'Error saving alarm' });
+        } else {
+            // adding custom fields
+            new_alarm.created = new Date();
+            new_alarm.modified = new Date();
+            new_alarm.status = 'open';
+            Alarm.addAlarm(new_alarm, function (err, new_alarm) {
+                if (err)
+                    res.send(err);
+                res.json(new_alarm);
+            });
+        }
     }
 };
 
